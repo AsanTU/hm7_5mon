fc3962e34982d06e91aea823621903c381dc2070
asant <asantursunaliev0606@gmail.com>
2024-01-25 17:17:16 +0300

    final

--- app/src/main/java/mbk/io/sabrina_hm7_5m/MainActivity.kt
+++ app/src/main/java/mbk/io/sabrina_hm7_5m/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSearch.setOnClickListener {
+        binding.btnSearch.setOnClickListener  {
             RetrofitService().api.getWeather(binding.etSearch.text.toString())
                 .enqueue(object : Callback<WeatherModel> {
                     @SuppressLint("SetTextI18n")
